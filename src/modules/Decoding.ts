cf4fda050b43c8ccf343e304d0947afc31a75254
mkrause <maikelkrause@gmail.com>
2020-06-01 00:12:25 +0200

    Add FlattenReporter.

--- src/modules/Decoding.ts
+++ src/modules/Decoding.ts
@@ -4,14 +4,20 @@ import * as ObjectUtil from '../util/ObjectUtil.js';
 import { Either } from '../util/Either.js';
 
 
+type LocationKey = unknown;
+
+
 //
 // Reporting
 //
 
-export type DecodeReport = { type : string };
+export type DecodeError = { type : string, given ?: unknown };
+export type DecodeReport = DecodeError | Array<{ key : LocationKey, reason : DecodeReport }>;
 
 export const fail : (report : DecodeReport) => Either<DecodeReport, never> = Either.left;
 export const success : <A>(value : A) => Either<never, A> = Either.right;
+
+export const unexpectedTypeError = <U>(given : U) : DecodeError => ({ type: 'unexpected-type', given });
 
 
 //
@@ -21,7 +27,7 @@ export const success : <A>(value : A) => Either<never, A> = Either.right;
 export type DecodeResult<A> = Either<DecodeReport, A>;
 
 export type Decoder<A> = {
-    decode : (input : unknown) => Either<DecodeReport, A>,
+    decode: (input : unknown) => Either<DecodeReport, A>,
 };
 
 export type TypeOf<D> = D extends Decoder<infer A> ? A : never;
@@ -32,60 +38,46 @@ export type TypeOf<D> = D extends Decoder<infer A> ? A : never;
 //
 
 export const unknown : Decoder<unknown> = {
-    decode : (input : unknown) => success(input),
+    decode: (input : unknown) => success(input),
 };
 
+export const neverError = { type: 'never' };
 export const never : Decoder<never> = {
-    decode : (input : unknown) => fail({ type: 'unexpected' }),
+    decode: (input : unknown) => fail(neverError),
 };
 
 export const unit : Decoder<null> = {
-    decode : (input : unknown) => {
-        if (input !== null) {
-            return fail({ type: 'unexpected-type', given: input });
-        } else {
-            return success(input);
-        }
+    decode: (input : unknown) => {
+        if (input !== null) { return fail(unexpectedTypeError(input)); }
+        return success(input);
     },
 };
 
 export const undef : Decoder<undefined> = {
-    decode : (input : unknown) => {
-        if (typeof input !== 'undefined') {
-            return fail({ type: 'unexpected-type', given: input });
-        } else {
-            return success(input);
-        }
+    decode: (input : unknown) => {
+        if (typeof input !== 'undefined') { return fail(unexpectedTypeError(input)); }
+        return success(input);
     },
 };
 
 export const string : Decoder<string> = {
-    decode : (input : unknown) => {
-        if (typeof input !== 'string') {
-            return fail({ type: 'unexpected-type', given: input });
-        } else {
-            return success(input);
-        }
+    decode: (input : unknown) => {
+        if (typeof input !== 'string') { return fail(unexpectedTypeError(input)); }
+        return success(input);
     },
 };
 
 export const number : Decoder<number> = {
-    decode : (input : unknown) => {
-        if (typeof input !== 'number') {
-            return fail({ type: 'unexpected-type', given: input });
-        } else {
-            return success(input);
-        }
+    decode: (input : unknown) => {
+        if (typeof input !== 'number') { return fail(unexpectedTypeError(input)); }
+        return success(input);
     },
 };
 
 export const boolean : Decoder<boolean> = {
-    decode : (input : unknown) => {
-        if (typeof input !== 'boolean') {
-            return fail({ type: 'unexpected-type', given: input });
-        } else {
-            return success(input);
-        }
+    decode: (input : unknown) => {
+        if (typeof input !== 'boolean') { return fail(unexpectedTypeError(input)); }
+        return success(input);
     },
 };
 
@@ -94,43 +86,50 @@ export const boolean : Decoder<boolean> = {
 // Complex constructors
 //
 
+type Tag = { tag : unknown };
+
 type Unknown = Decoder<unknown>;
 type RecordOf<T> = Record<PropertyKey, T>;
 type MapTypeOf<T> = { [key in keyof T] : TypeOf<T[key]> };
 
-export type ReportRecord<P extends RecordOf<Unknown>> = DecodeReport & (
-    | { type : 'unexpected-type', given : unknown }
-    | { type : 'keys-mismatch', keysMissing : Array<keyof P>, keysUnknown : Array<keyof P> }
-    | { type : 'props-invalid', invalidProps : { [prop in keyof P] ?: DecodeReport } }
-);
-export const record = <P extends RecordOf<Unknown>>(props : P) : Decoder<MapTypeOf<P>> => ({
-    decode : (input : unknown) => {
+
+export const record = <P extends RecordOf<Unknown>>(props : P) : Decoder<MapTypeOf<P>> & Tag => ({
+    tag: record,
+    decode: (input : unknown) => {
         type Instance = MapTypeOf<P>;
         
         if (typeof input !== 'object' || input === null) {
-            return fail({ type: 'unexpected-type', given: input } as ReportRecord<P>);
+            return fail(unexpectedTypeError(input));
         }
+        
+        const report : DecodeReport = [];
         
         const propsKeys = Object.keys(props);
         const inputKeys = Object.keys(input);
         
         // Check if the key sets are equal
         if (inputKeys.length !== propsKeys.length || !propsKeys.every(key => inputKeys.includes(key))) {
-            return fail({
-                type: 'keys-mismatch',
-                keysMissing: propsKeys.filter(key => !inputKeys.includes(key)),
-                keysUnknown: inputKeys.filter(key => !propsKeys.includes(key)),
-            } as ReportRecord<P>);
+            const keysMissing = propsKeys.filter(key => !inputKeys.includes(key));
+            const keysUnknown = inputKeys.filter(key => !propsKeys.includes(key));
+            
+            for (const key of keysMissing) {
+                report.push({ key, reason: { type: 'prop-missing' } });
+            }
+            for (const key of keysUnknown) {
+                report.push({ key, reason: { type: 'prop-unknown' } });
+            }
         }
-        
-        const inputWithKeys = input as { [key in keyof P] : unknown };
         
         // Check if all props are valid
         const instance = {} as Instance;
         const invalidProps : Record<PropertyKey, DecodeReport> = {};
         for (const propKey in props) {
+            if (!ObjectUtil.hasOwnProp(input, propKey)) {
+                continue;
+            }
+            
             const propDecoder = props[propKey] as Decoder<TypeOf<P[typeof propKey]>>;
-            const propInput = inputWithKeys[propKey];
+            const propInput = input[propKey];
             
             const propResult = propDecoder.decode(propInput);
             
@@ -144,10 +143,13 @@ export const record = <P extends RecordOf<Unknown>>(props : P) : Decoder<MapType
         }
         
         if (Object.keys(invalidProps).length > 0) {
-            return fail({
-                type: 'props-invalid',
-                invalidProps,
-            } as ReportRecord<P>);
+            for (const [key, reason] of Object.entries(invalidProps)) {
+                report.push({ key, reason });
+            }
+        }
+        
+        if (report.length > 0) {
+            return fail(report);
         }
         
         return success(instance);
@@ -155,7 +157,7 @@ export const record = <P extends RecordOf<Unknown>>(props : P) : Decoder<MapType
 });
 
 // export const variant = <P extends RecordOf<Unknown>>(alts : P) : Decoder<MapTypeOf<P>> => ({
-//     decode : (input : unknown) : input is MapTypeOf<P> => {
+//     decode: (input : unknown) : input is MapTypeOf<P> => {
 //         // TODO
 //     },
 // });
---
+++ src/reporters/FlattenReporter.ts
@@ -0,0 +1,41 @@
+
+import type { DecodeError, DecodeReport } from '../modules/Decoding.js';
+import * as D from '../modules/Decoding.js';
+
+
+type LocationKey = unknown;
+type Location = Array<LocationKey>;
+
+
+type DecodeErrorWithLocation = { location : Location, reason : DecodeError };
+
+const FlattenReporter = (report : DecodeReport) : Array<DecodeErrorWithLocation> => {
+    if (!Array.isArray(report)) {
+        return [{ location: [], reason: report }];
+    }
+    
+    const reasonsByKey = new Map<LocationKey, Array<DecodeReport>>();
+    
+    // Aggregate reasons by key
+    for (const reportItem of report) {
+        const key = reportItem.key;
+        
+        if (reasonsByKey.has(key)) {
+            reasonsByKey.get(key)!.push(reportItem.reason);
+        } else {
+            reasonsByKey.set(key, [reportItem.reason]);
+        }
+    }
+    
+    const errors : Array<DecodeErrorWithLocation> = [];
+    reasonsByKey.forEach((reportsForKey, key) => {
+        const childReports = reportsForKey.flatMap(FlattenReporter)
+            .map(report => ({ ...report, location: [key, ...report.location] }));
+        
+        errors.push(...childReports);
+    });
+    
+    return errors;
+};
+
+export default FlattenReporter;
